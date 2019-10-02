692261740acaa2a089ae95d62471237021333247
ananya_12 <49023871+Ananya-12@users.noreply.github.com>
2019-10-02 12:53:05 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,111 +1,62 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[72]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd
 import os
 import zipfile
-import seaborn as sn
+#import seaborn as sn
 
 
-# In[66]:
+d=pd.read_csv("/home/ananya/Documents/ananya/dsc/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
-
-with zipfile.ZipFile("/home/ananya/Documents/ananya/dsc/telco-customer-churn.zip","r") as z:
-    z.extractall(".")
-
-
-# In[14]:
-
-
-d=pd.read_csv("/home/ananya/Documents/ananya/dsc/telco-customer-churn.zip")
-d.head()
-
-
-# In[26]:
-
-
-d.info()
-
-
-# In[62]:
-
-
-d.describe()
-
-
-# In[ ]:
-
-
-
-
-
-# In[46]:
-
-
+print(d.head())
+print(d.info())
+print(d.describe())
 #droping the duplicate row 
 d=d.drop_duplicates(keep='first')
-
 shape=d.shape
 print('the number of categories is  and the total number of the customers are respectively')
 print(shape[1],shape[0])
-
-
-# In[54]:
-
-
 gen=d['gender'].value_counts()
 print('Total number of males and female are respectively', gen[0] ,' and ' , gen[1])
-
 sen=d['SeniorCitizen'].value_counts()
 print('Total no of senior citizen',sen[1])
-
 sen=d['PaperlessBilling'].value_counts()
 print('number of customer applyong for paperless billing ' , sen[0])
-
-
 #do the same for others aswell 
-
-
-# In[45]:
-
-
 sub=d.columns
 print('The column of the data are:' , )
-
-
-# In[55]:
-
-
 #checking for missing data 
 d.isnull().sum()
-
-
-# In[57]:
-
-
 #the type f data stored in each category 
-d.dtypes
+print(d.dtypes)
+print("gender:charge cost to them ")
+print(d.groupby(['gender','MonthlyCharges']).mean())
+print("monthly charges Median : ", d.MonthlyCharges.median())
+print("monthly charges Mean : ", d.MonthlyCharges.mode())
+print("monthly charges Mean : ", d.MonthlyCharges.mean())
+print("monthly charges Max : ", d.MonthlyCharges.max())
+print("monthly charges Min : ", d.MonthlyCharges.min())
 
 
-# In[1]:
+#data visualisation
+
+fig, ax = plt.subplots()
+ax.hist(d['tenure'],10)
+y1 = list(d.tenure)
+y2=list(d.MonthlyCharges) 
+plt.boxplot(y1)
+plt.boxplot(y2)
+fig, ax = plt.subplots()
+ax.hist(d['tenure'],10)
+plt.subplot(n,2,2)
+fig, ax = plt.subplots()
+ax.hist(d['MonthlyCharges'],10)
+plt.show()
 
 
-#fig, ax = plt.subplots()
-#ax.hist(d['tenure'],10)
 
 
-# In[2]:
 
-
-#sn.scatterplot(x='MonthlyCharges',y='tenure',data=d)
-
-
-# In[ ]:
 
 
 
